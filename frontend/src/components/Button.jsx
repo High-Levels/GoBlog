523c0f419a31caf91413a2d5d72ae18d38fd4211
Ujang Arisandi <ujangaja@gmail.com>
2023-03-27 16:29:26 +0700

    Style Login Page & Register

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({ label, variant, ...rest }) => {
+const Button = ({ label, variant, width, ...rest }) => {
   return (
     <div>
-      <button {...rest} className={`btn btn-${variant}`}>
+      <button {...rest} className={`btn btn-${variant} ${width}`}>
         {label}
       </button>
     </div>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,39 +2,55 @@ import React from 'react'
 import Button from '../components/Button'
 import Gap from '../components/Gap'
 import Input from '../components/Input'
+import ImageLogin from '../assets/images/Image-login.jpg'
+import '../style/index.css'
 
-const Register = () => {
+const Login = () => {
   return (
-    <div className='container-fluid h-100 mt-5'>
+    <div>
       <div className='row justify-content-center align-items-center h-100'>
-        <div className='col-md-4'>
-          <form action=''>
-            <div className='card'>
-              <div className='card-header'>Register Form</div>
-              <div className='card-body'>
-                <Input
-                  label='User Name'
-                  placeholder='Enter User Name'
-                  type='text'
-                />
-                <Gap height={18} />
-                <Input label='Email' placeholder='Enter Email' type='email' />
-                <Gap height={18} />
-                <Input
-                  label='Password'
-                  placeholder='Enter Password'
-                  type='password'
+        <div className='col-md-10 mt-5'>
+          <div className='card'>
+            <div className='row'>
+              <div className='col-md-8'>
+                <img
+                  src={ImageLogin}
+                  className='img-fluid rounded'
+                  style={{}}
                 />
               </div>
-              <div className='card-footer'>
-                <Button label='Register' variant='success' />
+              <div className='col-md-4 d-flex align-items-center'>
+                <div className='card-body me-4'>
+                  <form action=''>
+                    <h2 className='text-center'>Register </h2>
+                    <Input
+                      label='Username'
+                      placeholder='Enter Username'
+                      type='username'
+                    />
+                    <Gap height={18} />
+                    <Input
+                      label='Email'
+                      placeholder='Enter Email'
+                      type='email'
+                    />
+                    <Gap height={18} />
+                    <Input
+                      label='Password'
+                      placeholder='Enter Password'
+                      type='password'
+                    />
+                    <Gap height={18} />
+                    <Button label='Login' variant='success' width='w-100' />
+                  </form>
+                </div>
               </div>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
   )
 }
 
-export default Register
+export default Login
