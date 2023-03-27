f7cc4fef4d1bd61244a0c66b1e4f75ee48cf5365
mochsandhyka <moch.sandhyka@gmail.com>
2023-03-27 14:48:51 +0700

    auth

--- backend/app/controllers/auth.py
+++ backend/app/controllers/auth.py
@@ -1,8 +1,9 @@
 from app.models import db
 from app import responseHandler
 from flask import jsonify,request
+import flask
 import hashlib,os
-from flask_jwt_extended import create_refresh_token,create_access_token
+from flask_jwt_extended import create_refresh_token,create_access_token,set_access_cookies,get_jwt_identity,jwt_required,unset_jwt_cookies,unset_access_cookies,set_refresh_cookies,unset_refresh_cookies
 
 def login():
     try:
@@ -16,14 +17,13 @@ def login():
             }
         if user:
             accessToken = create_access_token(identity=userAuth,fresh=True)
-            refreshToken = create_refresh_token(identity=user)
-            response = {
-                "Data": userAuth,
+            refreshToken = create_refresh_token(identity=userAuth)
+            response = jsonify({
                 "accessToken": accessToken,
-                "refreshToken": refreshToken,
-                "Message": "Login Success"}
-            #set_access_cookies(jsonify(response), access_token)
-            return responseHandler.ok(response)
+                "Message": "Login Success"})
+            set_refresh_cookies(response, refreshToken)
+            set_access_cookies(response, accessToken)
+            return response
         else:
             response ={
                 "Message": "Username / Password is invalid"
@@ -33,4 +33,26 @@ def login():
         response ={
             "Error": str(err)
         }
+        return responseHandler.badGateway(response)
+    
+@jwt_required()
+def logout():
+    try:
+        currentUser = get_jwt_identity()
+        if currentUser:
+            response = {
+                "Message": "Logout Successfull"
+            }
+            unset_access_cookies(jsonify(response))
+            unset_refresh_cookies(jsonify(response))
+            return responseHandler.ok(response)
+        else:
+            response = {
+                "Message": "Logout Unccessfull"
+            }
+            return responseHandler.badRequest(response)
+    except Exception as err:
+        response = {
+                "Error": str(err)
+            }
         return responseHandler.badGateway(response)
