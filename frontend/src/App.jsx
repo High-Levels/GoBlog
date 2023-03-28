c2276425e563a4c7f090ed08c183a42386db76a9
Retno Wati <retnowati3099@gmail.com>
2023-03-28 08:34:48 +0700

    Update dikit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,4 @@
-import { Component, useState } from "react";
 import Particle from "./components/Particles.BG";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
-
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-// import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -18,7 +7,6 @@ import Navbar from "./components/Navbar";
 import NewStory from "./pages/NewStory";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <>
       <Particle />
@@ -31,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} exact />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/new-story" element={<NewStory />} />
         </Routes>
       </Router>
     </>
