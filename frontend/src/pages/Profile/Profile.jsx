46df8618d6905050531748be970273cd747d5962
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2023-04-07 01:49:06 +0700

    tampilan_awal_profile

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import CardProfile from "../../components/CardProfile";
 import { Link, useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  Navigate = useNavigate()
+
   return (
     <div className="container-fluid">
+      <CardProfile />
       <hr />
       <header className="d-flex align-items-center justify-content-md-between p-3 mb-4 border-bottom">
         <h3 className="mt-2">Your Stories</h3>
--- frontend/src/pages/Profile/Published.jsx
+++ frontend/src/pages/Profile/Published.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '../../components/Card';
 import Profile from './Profile';
+import Gap from '../../components/Gap';
 
 export const Published = () => {
   return (
     <div>
     <Profile />
+      <Card />
+      <Gap />
       <Card />
     </div>
   )
