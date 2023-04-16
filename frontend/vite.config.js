39eef99f43d59928b3fd239e0c767824bc26b1b7
Ujang Arisandi <ujangaja@gmail.com>
2023-04-16 21:59:44 +0700

    setup proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://13.239.136.211',
+    },
+  },
   plugins: [react()],
 })
