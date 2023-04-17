fbf3acf83d843c45145569baf665a698712c54cf
Ujang Arisandi <ujangaja@gmail.com>
2023-04-17 21:31:18 +0700

    Title Page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import GoblogPict from '../assets/images/Goblog.jpeg'
 import Avatar from './Avatar'
 import Image1 from '../assets/images/avatar.png'
 import '../style/index.css'
 import Search from './Search'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { SetPageTitle } from '../utils/SetPageTitle'
 
 const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(
@@ -24,7 +25,13 @@ const Navbar = () => {
       console.log(error)
     }
   }
+  // memasang use location
+  const location = useLocation()
 
+  // panggil setPageTitle
+  useEffect(() => {
+    SetPageTitle(location.pathname)
+  }, [location.pathname])
   return (
     <div>
       <nav className='navbar navbar-expand-sm navbar-light bg-light bg-opacity-75 fixed-top'>
@@ -44,26 +51,28 @@ const Navbar = () => {
               <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
             </svg>
           </a>
-          
+
           <form className='form-inline my-2 my-sm-0 d-none d-sm-block'>
             <Search />
           </form>
-          {isLoggedIn ?(
-          <div>
-            <a href='/new-story' className='btn border-0 '>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='16'
-                height='16'
-                fill='currentColor'
-                className='bi bi-pen'
-                viewBox='0 0 16 16'
-              >
-                <path d='m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z' />
-              </svg>
-            </a>
-          </div>
-          ):(<div></div>)}
+          {isLoggedIn ? (
+            <div>
+              <a href='/new-story' className='btn border-0 '>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='16'
+                  height='16'
+                  fill='currentColor'
+                  className='bi bi-pen'
+                  viewBox='0 0 16 16'
+                >
+                  <path d='m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z' />
+                </svg>
+              </a>
+            </div>
+          ) : (
+            <div></div>
+          )}
           <div className='dropdown'>
             <button
               className='btn dropdown-toggle border-0'
