9bf724caee161180ff3233805ae57aea20c8c543
Retno Wati <retnowati3099@gmail.com>
2023-04-17 22:49:11 +0700

    Menambahkan navigate pada button write a story

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -8,6 +8,10 @@ import { Link, useNavigate } from "react-router-dom";
 import "./profile.css";
 
 const Profile = () => {
+  const navigate = useNavigate();
+  const handleWriteStory = () => {
+    navigate("/new-story");
+  };
   return (
     <div className="container-fluid">
       <div className="prof">
@@ -16,7 +20,11 @@ const Profile = () => {
       <header className="d-flex align-items-center justify-content-md-between p-3 mt-4 mb-4 border-bottom">
         <h3 className="mt-2">Your Stories</h3>
         <div className="ms-auto d-flex gap-2 p-3">
-          <Button label="Write a story" variant="secondary" />
+          <Button
+            label="Write a story"
+            variant="secondary"
+            onClick={handleWriteStory}
+          />
           <Button label="Edit" variant="light" />
         </div>
       </header>
